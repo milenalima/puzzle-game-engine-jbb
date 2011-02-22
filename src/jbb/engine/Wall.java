ad1ebbd9a778957d3156647d9c79dae8df644ba1
bionine <bionine@connect.carleton.ca>
2011-02-22 19:48:36 +0000

    Committing the Tile, Board and Wall classes.

---
+++ src/jbb/engine/Wall.java
@@ -0,0 +1,18 @@
+/**
+ * The wall is a tile which blocks the path of an Avatar. It is pictorially represented
+ * by a Wall, and it simply a Tile that is not accessible (generally).
+ * @author Bruno Colantonio
+ */
+package jbb.engine;
+
+public class Wall extends Tile{
+
+	/**
+	 * The constructor will create a wall, or in other words a tile that is not accessible
+	 */
+
+	public Wall(Position position, Board board) {
+		super(position, board);
+		setAccessible(false);
+	}
+}
