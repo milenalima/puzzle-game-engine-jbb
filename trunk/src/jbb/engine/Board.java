65bc7a8b7af18702135a65fb9d31f7330ddb310c
bionine@connect.carleton.ca <bionine@connect.carleton.ca@3bf74f1a-fba9-1258-d82a-c52c999313a4>
2011-02-22 19:48:36 +0000

    Committing the Tile, Board and Wall classes.

---
+++ trunk/src/jbb/engine/Board.java
@@ -0,0 +1,131 @@
+/**
+ * This abstract class contains a two-dimensional array of Tiles, that
+ * comprises the playing field of a game. It handles an individual turn of the game. 
+ * The class will be extended by game-specific board classes, 
+ * which will implement the populatePlayingField()method to populate 
+ * the board as required by the game.
+ * @author Boris Ionine
+ */
+package jbb.engine;
+
+import java.util.ArrayList; 
+
+public class Board {
+	private Tile[][] playingField;
+	private Tile[][] itemMap;
+	private int width;
+	private int height;
+	
+	private ArrayList<Avatar> moveableTiles;
+	
+	/**
+	 * Constructor. Initialises instance variables 
+	 * width, height and playingField. Calls populatePlayingField() to 
+	 * set up the board. 
+	 * @param width Width of board
+	 * @param height Height of board
+	 */
+	public Board(int width, int height)
+	{
+		this.width = width;
+		this.height = height;
+		playingField = new Tile[width][height];	
+		itemMap = new Tile[width][height];	
+		populatePlayingField();
+		moveableTiles = new ArrayList<Avatar>();
+	}
+	
+	/**
+	 * Abstract method to be implemented by game-specific classes that 
+	 * extend Board. The method will populate the board with tiles.
+	 */
+	private void populatePlayingField()
+	{
+		for(int i = 0; i < width; i++)
+		{
+			for(int j = 0; j < height; j++)
+			{
+				playingField[i][j] = new Tile(new Position(i,j), this);
+				itemMap[i][j] = new Tile(new Position(i,j), this);
+			}
+		}
+	}
+	
+	/**
+	 * This method will handle a turn in the game.
+	 * @param The argument position is the Position of the Tile the player selected.
+	 */
+	public void playTurn(Position position)
+	{
+		boolean itemPickedUp = false;
+		//Hero is always the last element of the ArrayList
+		Avatar hero = moveableTiles.get(moveableTiles.size());
+		for(int i = 0; i<moveableTiles.size()-1; i++)
+		{
+			//check if an item was picked up by an NPC
+			itemPickedUp = moveableTiles.get(i).moveTo(hero.getPosition());
+			if(itemPickedUp)
+			{	
+				//remove item from itemMap
+				Position current = moveableTiles.get(i).getPosition();
+				itemMap[current.getRow()][current.getCol()] = new Tile(current,this);
+			}
+		}
+		//check if an item was picked up by the Hero
+		itemPickedUp = hero.moveTo(position); 
+		if(itemPickedUp)
+		{
+			//remove item from itemMap
+			Position current = hero.getPosition();
+			itemMap[current.getRow()][current.getCol()] = new Tile(current,this);
+		}
+		syncItemMapAndField();
+	}
+	public void syncItemMapAndField()
+	{
+		for(int i = 0; i < width; i++)
+		{
+			for(int j = 0; j < height; j++)
+			{
+				if(itemMap[i][j] instanceof Item)
+					playingField[i][j] = itemMap[i][j];
+			}
+		}
+		
+		for(Avatar mT: moveableTiles)
+		{
+			playingField[mT.getPosition().getRow()][mT.getPosition().getCol()] = mT;
+		}
+	}
+	/**
+	 * Returns the width of the board.
+	 * @return width of board
+	 */
+	public int getWidth()
+	{
+		return width;
+	}
+	
+	/**
+	 * Returns the height of the board.
+	 * @return height of board
+	 */
+	public int getHeight()
+	{
+		return height;
+	}
+	
+	/**
+	 * Returns the tile at said position or throws an exception if the position is invalid.
+	 * @param position The position of interest
+	 * @return tile at position
+	 */
+	public Tile getTile(Position position) throws IndexOutOfBoundsException{
+		if(position.getRow() < 0 || position.getRow() >= width)
+			throw new IndexOutOfBoundsException("Row out of bounds.");
+		else if(position.getCol() < 0 || position.getCol() >= height)
+			throw new IndexOutOfBoundsException("Col out of bounds");
+		else
+			return playingField[position.getRow()][position.getCol()];
+	}
+}
---
+++ trunk/src/jbb/engine/Tile.java
@@ -0,0 +1,135 @@
+/**
+ * The Tile class has a position and an image. 
+ * All squares on the board will be instances of Tile or one of its subclasses.
+ * The tile will either be blank or hold an image.
+ * @author Bruno Colantonio...and Boris
+ */
+package jbb.engine;
+import javax.swing.ImageIcon;
+
+
+public class Tile{
+	
+	public static final int TOP_LEFT=0; 
+	public static final int TOP=1;
+	public static final int TOP_RIGHT=2;
+	public static final int LEFT=3;
+	public static final int RIGHT=4;
+	public static final int BOTTOM_LEFT=5;
+	public static final int BOTTOM=6;
+	public static final int BOTTOM_RIGHT=7;
+	
+	private ImageIcon image; 
+	private boolean accessible;
+	
+	protected Position position;
+	
+	protected Board board;
+	
+	/**
+	 * The constructor will create a Tile at the positions of
+	 * the row and column. The image of a standard Tile is blank.
+	 * @param position will set the position of the Tile
+	 * @param board will set the Tile to the specified board
+	 */
+	public Tile(Position position, Board board){
+		accessible = true;
+		image = new ImageIcon();
+		setPosition(position);
+		this.board = board;
+	}
+	
+	/**
+	 * The method will return the Position of the Tile
+	 * @return the position of the tile
+	 */
+	public Position getPosition(){
+		return position;
+	}
+	
+	/**
+	 * The method will set the position to the parameter given. 
+	 * Throws an exception if the position given is out of bounds.
+	 * @param position to set piece
+	 */
+	public void setPosition(Position position) throws IndexOutOfBoundsException{
+		if(position.getRow() < 0 || position.getRow() >= board.getWidth())
+			throw new IndexOutOfBoundsException("Row out of bounds.");
+		else if(position.getCol() < 0 || position.getCol() >= board.getHeight())
+			throw new IndexOutOfBoundsException("Col out of bounds");
+		else
+			this.position = position;
+	}
+	
+	/**
+	 * The method will return the image used on the tile.
+	 * @return the image used on the tile.
+	 */
+	public ImageIcon getImage(){
+		return image;
+	}
+	
+	/**
+	 * The method will set the image on the tile.
+	 * @param image The image that is to be used on the tile.
+	 */
+	public void setImage(ImageIcon image){
+		this.image = image;
+	}
+	
+	/**
+	 * The method will return true if the tile is able to play
+	 * and false if cannot play
+	 * @return true if the tile is accessible, false otherwise
+	 */
+	public boolean getAccessible(){
+		return accessible;
+	}
+	
+	/**
+	 * The method will make the tile accessible
+	 * @param accessible Set whether the tile is accessible
+	 * by the Avatars or not.
+	 */
+	public void setAccessible(boolean accessible){
+		this.accessible = accessible;
+	}
+	
+	/**
+	 * The method will return an array to show what tiles
+	 * are surrounding the tile in parameter.
+	 * The indexes of the the array returned will correspond
+	 * with the positions of the elements (i.e. will be accessible
+	 * using the constants defined at the top of class).
+	 * Adjacent squares that are out of the bounds of the board 
+	 * will be set to null.
+	 */
+	public Tile[] getAdjacentTiles(){
+		Tile[] adj = new Tile[8];
+		int adjIndex = 0;
+		int thisRow = position.getRow();
+		int thisCol = position.getCol();
+		for(int i = -1; i < 1; i++)
+		{			
+			for(int j = -1; j < 1; j++)
+			{
+				if(i==0 && j==0)//if the loop is at the centre Tile, skip it
+				{
+					j++;
+				}
+				
+				//out of bounds checks
+				if((thisRow + i) < 0 || (thisCol + j) < 0)
+					adj[adjIndex] = null;
+				else if((thisRow + i) >= board.getWidth() || (thisCol + j) >= board.getHeight())
+					adj[adjIndex] = null;
+				//add Tile to appropriate index
+				else
+					adj[adjIndex] = board.getTile(new Position(thisRow+i,thisCol+j));
+				
+				adjIndex++;
+			}
+		}
+		return new Tile[0];
+	}
+}
