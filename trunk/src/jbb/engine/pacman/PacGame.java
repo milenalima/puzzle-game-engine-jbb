504b096c307fb70a2094ce98fce5fa8925be3f8a
gravel.j@live.ca <gravel.j@live.ca@3bf74f1a-fba9-1258-d82a-c52c999313a4>
2011-02-23 00:18:24 +0000

    Created PacGame which allows text based input to move PacMan around. Lots of bugs

---
+++ trunk/src/jbb/engine/pacman/PacGame.java
@@ -0,0 +1,41 @@
+package jbb.engine.pacman;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import jbb.engine.Position;
+
+public class PacGame {
+	
+	private PacWorld board;
+	
+	public PacGame() {
+		board = new PacWorld();
+	}
+	
+	public void play() {
+		Position newPos;
+		Scanner s;
+		System.out.println("You must play until the end");
+		while (true) {
+			System.out.println(board);
+			System.out.print("Type next position in form [row] [int]: ");
+			s = new Scanner(System.in);
+			try {
+				newPos = new Position(s.nextInt(),s.nextInt());
+				board.playTurn(newPos);
+			} catch (IllegalArgumentException iae) {
+				System.out.println(iae.getMessage());
+			} catch (IndexOutOfBoundsException ioobe) {
+				System.out.println(ioobe.getMessage());
+			} catch (InputMismatchException ime) {
+				System.out.println("Invalid Input)");
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		PacGame game = new PacGame();
+		game.play();
+	}
+}
