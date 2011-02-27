35f309ac395a09ab0fa60115e7dda5b137d3eb64
gravel.j@live.ca <gravel.j@live.ca@3bf74f1a-fba9-1258-d82a-c52c999313a4>
2011-02-27 16:29:18 +0000

    Change to MovableTiles: The Hero is now always first (see play(...) method in Board), and the populatePlayingField method uses the itemMap as well as the movableTiles list to generate the appropriate setup. This allows for items to be placed under NPCs at start. PacMan works, but no win/lose conditions. Next step is building a bigger map to better test the ghosts.

--- trunk/src/jbb/engine/mouseland/MouseLand.java
+++ trunk/src/jbb/engine/mouseland/MouseLand.java
@@ -1,7 +1,10 @@
 package jbb.engine.mouseland;
 
 
-import jbb.engine.*;
+import jbb.engine.Board;
+import jbb.engine.Position;
+import jbb.engine.Tile;
+import jbb.engine.Wall;
 
 public class MouseLand extends Board{
 	public static final int WIDTH = 5;
@@ -9,43 +12,42 @@ public class MouseLand extends Board{
 	
 	public MouseLand() {
 		super(WIDTH, HEIGHT);
+		movableTiles.add(new MouseHero(new Position(4,2), this));
+		movableTiles.add(new Mouse(new Position(2,2), this));
+		populatePlayingField(movableTiles);
 	}
 	
 	@Override
-	protected void populatePlayingField()
-	{
-		// Hero is always at the end of the list;
-		moveableTiles.add(new Mouse(new Position(2,2), this));
-		moveableTiles.add(new MouseHero(new Position(4,2), this));
-		playingField[0][0] = new Tile(new Position(0,0),this);
-		playingField[0][1] = new Tile(new Position(0,1),this);
-		playingField[0][2] = new Tile(new Position(0,2),this);
-		playingField[0][3] = new Tile(new Position(0,3),this);
-		playingField[0][4] = new Tile(new Position(0,4),this);
+	protected void populateItemMap() {
+		itemMap[0][0] = new Tile(new Position(0,0),this);
+		itemMap[0][1] = new Tile(new Position(0,1),this);
+		itemMap[0][2] = new Tile(new Position(0,2),this);
+		itemMap[0][3] = new Tile(new Position(0,3),this);
+		itemMap[0][4] = new Tile(new Position(0,4),this);
 		
-		playingField[1][0] = new Tile(new Position(1,0),this);
-		playingField[1][1] = new Wall(new Position(1,1),this);
-		playingField[1][2] = new Wall(new Position(1,2),this);
-		playingField[1][3] = new Wall(new Position(1,3),this);
-		playingField[1][4] = new Tile(new Position(1,4),this);
+		itemMap[1][0] = new Tile(new Position(1,0),this);
+		itemMap[1][1] = new Wall(new Position(1,1),this);
+		itemMap[1][2] = new Wall(new Position(1,2),this);
+		itemMap[1][3] = new Wall(new Position(1,3),this);
+		itemMap[1][4] = new Tile(new Position(1,4),this);
 		
-		playingField[2][0] = new Tile(new Position(2,0),this);
-		playingField[2][1] = new Tile(new Position(2,1),this);
-		playingField[2][2] = new Mouse(new Position(2,2),this);
-		playingField[2][3] = new Tile(new Position(2,3),this);
-		playingField[2][4] = new Tile(new Position(2,4),this);
+		itemMap[2][0] = new Tile(new Position(2,0),this);
+		itemMap[2][1] = new Tile(new Position(2,1),this);
+		itemMap[2][2] = new Tile(new Position(2,2),this);
+		itemMap[2][3] = new Tile(new Position(2,3),this);
+		itemMap[2][4] = new Tile(new Position(2,4),this);
 
-		playingField[3][0] = new Tile(new Position(3,0),this);
-		playingField[3][1] = new Wall(new Position(3,1),this);
-		playingField[3][2] = new Wall(new Position(3,2),this);
-		playingField[3][3] = new Wall(new Position(3,3),this);
-		playingField[3][4] = new Tile(new Position(3,4),this);
+		itemMap[3][0] = new Tile(new Position(3,0),this);
+		itemMap[3][1] = new Wall(new Position(3,1),this);
+		itemMap[3][2] = new Wall(new Position(3,2),this);
+		itemMap[3][3] = new Wall(new Position(3,3),this);
+		itemMap[3][4] = new Tile(new Position(3,4),this);
 		
-		playingField[4][0] = new Tile(new Position(4,0),this);
-		playingField[4][1] = new Tile(new Position(4,1),this);
-		playingField[4][2] = moveableTiles.get(moveableTiles.size()-1);
-		playingField[4][3] = new Tile(new Position(4,3),this);
-		playingField[4][4] = new Tile(new Position(4,4),this);
+		itemMap[4][0] = new Tile(new Position(4,0),this);
+		itemMap[4][1] = new Tile(new Position(4,1),this);
+		itemMap[4][2] = new Tile(new Position(4,2),this);
+		itemMap[4][3] = new Tile(new Position(4,3),this);
+		itemMap[4][4] = new Tile(new Position(4,4),this);
 	}
 	
 	public static void main(String[] args) {
