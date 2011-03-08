2e467b70c3c7fead56513981201802232e477629
gravel.j@live.ca <gravel.j@live.ca@3bf74f1a-fba9-1258-d82a-c52c999313a4>
2011-03-08 17:45:59 +0000

    GameViews should mostly work... PacMan displays all the content on the board. ISSUES: 	Avatars don't move properly in GUI, but they do in the text game. 	A white tile is seen after PacMan moves the first time, and stay there for the rest of the game
    NEW FEATURE:
    	The Tile has a static method setBlankColor which changes the tile color for all tiles. this needs to be set before calling populatePlayingField.

--- trunk/src/jbb/engine/pacman/Ghost.java
+++ trunk/src/jbb/engine/pacman/Ghost.java
@@ -23,7 +23,7 @@ public class Ghost extends NPC{
 	 * @param board
 	 */
 	public Ghost(Position position, Board board) {
-		super(new ImageIcon("img/mouseTrap.png"), LIVES, position, board);
+		super(new ImageIcon("img/Ghost-D.png"), LIVES, position, board);
 		// TODO Auto-generated constructor stub
 	}
 	
