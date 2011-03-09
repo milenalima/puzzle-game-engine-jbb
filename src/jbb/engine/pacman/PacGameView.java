5b639f3e08f9b72d2827c4266c09da54797e7e34
gravel.j <gravel.j@live.ca>
2011-03-09 21:04:50 +0000

    tried to display important info, such as lives, points.... (game dependent), commented out the changes for now
    for pacman:
    	I need to implement what happens to the look when pacman picks up a power pellet
    	I need to implement how the different ghosts react
    	
    for the board:
    	I need to display important info, such as lives, points.... (game dependent)
    	Weird bug when pressing on the corner walls (???) causes movement to be different than what is expected.

--- src/jbb/engine/pacman/PacGameView.java
+++ src/jbb/engine/pacman/PacGameView.java
@@ -1,12 +1,21 @@
 package jbb.engine.pacman;
 
+import java.awt.Component;
+
+import javax.swing.JTextField;
+
 import jbb.engine.BoardView;
-import jbb.engine.Tile;
 
 public class PacGameView extends BoardView {
 
 	public PacGameView(PacWorld board) {
 		super(board);
+		/*
+		JTextField lives = new JTextField("LIVES...");
+		JTextField points = new JTextField("POINTS...");
+		Component[] more = {lives, points};
+		addMoreComponents(more);
+		*/
 	}
 	
 	public static void main(String[] args) {
