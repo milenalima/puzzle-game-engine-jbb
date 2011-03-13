9ea073f543ce4e6351b300a0a55cf70cda2c4b59
gravel.j@live.ca <gravel.j@live.ca@3bf74f1a-fba9-1258-d82a-c52c999313a4>
2011-03-13 23:05:07 +0000

    PacMan done.
    RESOLVED:
    for PacMan:
    	Ghosts have different movement mechanics
    	Sam ghost reacts to PacMan being invincible
    	PacMan changes direction based on which direction he's moving in
    	
    TODO:
    for MouseLand:
    	Maybe make a map that allows for more than one way of winning?
    	(Minor) When a mouse dies it should stay on the board as a MouseTrap with dead mouse
    	(Minor) When MouseHero walks onto a placed MouseTrap, he should pick it up (?regardless of if it killed a mouse or not?)
    	(Minor) Change the direction Mice are facing when they move.
    
    for Pipes:
    	CRITICAL! I noticed a huuuuge lag when the water leaked out of the board (when win conditions weren't changed). I'm not sure 		if this applies to the game when it is functioning correctly, but it will be something to check for.
    	Randomly generate the next tile (can add component at top to say what is next tile)
    	Make board bigger? to allow for more room for components at the top, and more variation of play :)
    	(I'm really loving this game. can't wait for it to be finished! You have no idea how hard it is for me not to go ahead and 	just get'er'done D: haha)
    	
    for all:
    	When a user quits a game, it should ask if the user wants to play another game.

--- trunk/src/jbb/engine/pacman/Sam.java
+++ trunk/src/jbb/engine/pacman/Sam.java
@@ -15,11 +15,36 @@ public class Sam extends Ghost {
 	}
 	
 	@Override
+	/**
+	 * this ghost remembers a previous move of PacMan and tries to go where
+	 * pacman will be, rather than where he was. If PacMan becomes invulnerable,
+	 * this ghost runs away.
+	 */
 	public Position getNextPosition(Position position) {
-		if (lastPosition != null) {
-			
+		if (!((PacMan)(board.getHero())).isInvulnerable()) {
+			if (lastPosition != null) {
+				if (position.isNorthOf(lastPosition)) {
+					lastPosition = position;
+					return getNextPosition(new Position(position.getRow()-1,position.getCol()));
+				} else if (position.isSouthOf(lastPosition)) {
+					lastPosition = position;
+					return getNextPosition(new Position(position.getRow()+1,position.getCol()));
+				} else if (position.isEastOf(lastPosition)) {
+					lastPosition = position;
+					return getNextPosition(new Position(position.getRow(),position.getCol()+1));
+				} else if (position.isWestOf(lastPosition)) {
+					lastPosition = position;
+					return getNextPosition(new Position(position.getRow(),position.getCol()-1));
+				}
+			} return super.getNextPosition(position);
+		} else { // PacMan is invulnerable, so run away!
+			// run to the opposite side of the board
+			int runCol = Math.abs(board.getWidth()-position.getCol());
+			int runRow = Math.abs(board.getHeight()-position.getRow());
+			Position runPosition = new Position(runRow,runCol);
+			return super.getNextPosition(runPosition);
 		}
-		return super.getNextPosition(position);
+		// otherwise just try to move towards given position
 	}
 
 }
