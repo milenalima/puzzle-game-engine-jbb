d63c86a504acd69ffae869f529b710d208ad4635
gravel.j@live.ca <gravel.j@live.ca@3bf74f1a-fba9-1258-d82a-c52c999313a4>
2011-03-29 16:03:13 +0000

    The Android application will be written in this directory

---
+++ PuzzleGameEngine/src/jbb/android/GameLauncher.java
@@ -0,0 +1,13 @@
+package jbb.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameLauncher extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
