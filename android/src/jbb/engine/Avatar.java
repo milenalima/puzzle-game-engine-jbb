2a951dc416ce04e945d28f6750abd5276298c0e0
gravel.j@live.ca <gravel.j@live.ca@3bf74f1a-fba9-1258-d82a-c52c999313a4>
2011-04-05 01:54:55 +0000

    EVERYTHING WORKS  except undo/redo

---
+++ android/src/jbb/engine/Avatar.java
@@ -0,0 +1,193 @@
+package jbb.engine;
+
+
+/**
+ * The Abstract Avatar class defines the controllable tile on the board: it is the only one that will be moved.
+ * Avatar may pick up items, but not go through walls (generally). The behaviour is further developed in the subclasses Hero and NPC.
+ * If an avatar is to die, it is to die upon collision, thus there is no need for hitPoints.
+ * @author Jonathan Gravel
+ */
+
+public abstract class Avatar extends Tile {
+	
+	private int lives;
+	
+	/**
+	 * @param image pictorial representation of the Avatar to be used on a Tile
+	 * @param lives represents the starting number of lives of the Avatar
+	 * @param position represents the position of the Avatar on the Board
+	 * @param board represents the board that is associated to this Avatar
+	 */
+	public Avatar(int lives, Position position, Board board) {
+		super(position, board);
+		this.lives = lives;
+	}
+	
+	/**
+	 * This function checks to see if there is an item that can be picked up by
+	 * the Avatar at the position.
+	 * 
+	 * @param position
+	 * @return true if the object can be picked up by Avatar
+	 */
+	public abstract boolean hasGoodie(Position position);
+	
+	/**
+	 * Gets the next position to move to given a position. The position returned
+	 * will be either up, down, left or right of the avatar.
+	 * 
+	 * @param position to move to or towards
+	 * @return Position calculated
+	 * @throws IllegalArgumentException if move is out of bounds, or if the Avatar
+	 * will move into a wall.
+	 */
+	public Position getNextPosition(Position position) throws IllegalArgumentException{
+		try {
+			if (canMoveTo(position)) {
+				return position;
+			}
+		} catch (ArrayIndexOutOfBoundsException exc) {
+			throw new IllegalArgumentException("Not a possible move");
+		}
+		// determine which direction is wanted
+		/*
+		 * + - - - - - - - - - -
+		 * + + - - - - - - - - +
+		 * + + + - - - - - - + +
+		 * + + + + - - - - + + +
+		 * + + + + + - - + + + +
+		 * + + + + +   + + + + +
+		 * + + + + - - + + + + +
+		 * + + + - - - - + + + +
+		 * + + - - - - - - + + +
+		 * + - - - - - - - - + +
+		 * - - - - - - - - - - +
+		 * 
+		 * The pluses and minuses indicate which direction (up,down,left,right)
+		 * to move in depending on where the user clicked.
+		 */
+		int gotoRow = position.getRow();
+		int gotoCol = position.getCol();
+		int myRow = this.position.getRow();
+		int myCol = this.position.getCol();
+		if (myRow == gotoRow && myCol == gotoCol) {
+			throw new IllegalArgumentException("Cannot move to same position");
+		}
+		try {
+			if (gotoRow <= myRow) { // we are moving upwards
+				if (gotoCol <= myCol) { // we are moving left or up
+					if (myCol-gotoCol >= myRow-gotoRow) return getNextPosition(LEFT);
+					else return getNextPosition(TOP);
+				} else { // we are moving right or up
+					if (gotoCol-myCol > myRow-gotoRow) return getNextPosition(RIGHT);
+					else return getNextPosition(TOP);
+				}
+			} else { // we are moving downwards
+				if (gotoCol <= myCol) { // we are moving left or down
+					if (myCol-gotoCol > gotoRow-myRow) return getNextPosition(LEFT);
+					else return getNextPosition(BOTTOM);
+				} else { // we are moving right or down
+					if (gotoCol-myCol > gotoRow-myRow) return getNextPosition(RIGHT);
+					else return getNextPosition(BOTTOM);
+				}
+			}
+		} catch (IllegalArgumentException exc) {
+			throw new IllegalArgumentException("Not a possible move");
+		}
+	}
+	
+	/**
+	 * Gets the next position to move to given a direction. The position returned
+	 * will be either up, down, left or right of the avatar.
+	 * 
+	 * @param position position to move to or towards
+	 * @return Position calculated
+	 * @throws IllegalArgumentException if move is out of bounds, or if the Avatar
+	 * will move into a wall.
+	 */
+	protected Position getNextPosition(int direction) throws IllegalArgumentException {
+		Position newPos;
+		switch (direction) {
+			case TOP:
+				newPos = new Position(position.getRow()-1, position.getCol());
+				if (!this.canMoveTo(newPos)) throw(new IllegalArgumentException("Move is not permitted"));
+				return newPos;
+			case BOTTOM:
+				newPos = new Position(position.getRow()+1, position.getCol());
+				if (!this.canMoveTo(newPos)) throw(new IllegalArgumentException("Move is not permitted"));
+				return newPos;
+			case LEFT:
+				newPos = new Position(position.getRow(), position.getCol()-1);
+				if (!this.canMoveTo(newPos)) throw(new IllegalArgumentException("Move is not permitted"));
+				return newPos;
+			case RIGHT:
+				newPos = new Position(position.getRow(), position.getCol()+1);
+				if (!this.canMoveTo(newPos)) throw(new IllegalArgumentException("Move is not permitted"));
+				return newPos;
+			default:
+				throw(new IllegalArgumentException("Invalid direction"));
+		}
+	}
+
+	/**
+	 * Determines if the Tile at that position is Accessible and is adjacent to the current Position
+	 * 
+	 * @param position
+	 * @return false if the position not accessible, true if if the new position is possible
+	 */
+	public boolean canMoveTo(Position position) {
+		if (position.getRow() < 0 || position.getCol() < 0
+				|| position.getRow() >= board.getHeight() || position.getCol() >= board.getWidth())
+			return false;
+		else if (!board.getTile(position).getAccessible()) return false;
+		else if ((Math.abs(position.getRow() - this.position.getRow()) == 1 && Math.abs(position.getCol() - this.position.getCol()) == 0))
+			return true; // moved one space vertically
+		else if ((Math.abs(position.getRow() - this.position.getRow()) == 0 && Math.abs(position.getCol() - this.position.getCol()) == 1))
+			return true; // moved one space horizontally
+		else return false;
+	}
+	
+	/**
+	 * Get number of lives remaining
+	 * 
+	 * @return lives
+	 */
+	public int getLives() {
+		return lives;
+	}
+	
+	/**
+	 * Increment lives by 1
+	 */
+	public void addLife() {
+		lives++;
+	}
+	
+	/**
+	 * Decrement lives by 1
+	 * 
+	 * @return true if the number of lives has reached 0 or less
+	 */
+	public boolean removeLife() {
+		lives--;
+		if (lives == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Sets the number of lives
+	 */
+	public void setLives(int lives) {
+		this.lives=lives;
+	}
+	/**
+	 * determine what happens when the avatar collides with another
+	 * 
+	 * @param avatar
+	 * @return true if the character dies
+	 */
+	public abstract boolean collidesWith(Avatar avatar);
+	
+}
---
+++ android/src/jbb/engine/pipes/PipeMap.java
@@ -0,0 +1,313 @@
+package jbb.engine.pipes;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import jbb.engine.Avatar;
+import jbb.engine.Board;
+import jbb.engine.Item;
+import jbb.engine.Position;
+import jbb.engine.Tile;
+import jbb.engine.Wall;
+
+/**
+ * Board class for the Pipes game. 
+ * Manages the pieces when a turn is played.
+ * @author Boris Ionine
+ */
+public class PipeMap extends Board{
+	private static final int MAP_ONE = 1;
+	private static final int MAP_TWO = 2;
+	private static final int MAP_THREE = 3;	
+	public static final int WATER_START_TURN = 60;
+	public static final int WIDTH = 15;
+	public static final int HEIGHT = 15;
+	public Position winningPosition;
+	private int turnsUntilWater = WATER_START_TURN;
+	private int numTurns = 0;
+	private boolean runWater = false;
+	private static int map=1;
+	
+	public PipeMap(InputStream[] is){
+		super(WIDTH, HEIGHT, is);
+		syncItemMapAndField(movableTiles);
+	}
+	
+	@Override
+	protected void populateItemMap() {
+		if(map == MAP_ONE){
+			movableTiles.add(new Plumber(new Position(1,1), this));
+			movableTiles.add(new Water(new Position(1,1), this));
+			winningPosition = new Position(13,14);
+		}
+		else if(map == MAP_TWO){
+			movableTiles.add(new Plumber(new Position(3,1), this));
+			movableTiles.add(new Water(new Position(3,1), this));
+			winningPosition = new Position(9,14);
+		}
+		else if(map == MAP_THREE){
+			movableTiles.add(new Plumber(new Position(2,1), this));
+			movableTiles.add(new Water(new Position(2,1), this));
+			winningPosition = new Position(13,14);
+
+		}
+
+		Scanner s;
+		try {
+			is[map-1].reset();
+		} catch (IOException e) {
+			// shouldn't happen
+		}
+		s = new Scanner(is[map-1]);
+		s.useDelimiter("\n");
+		char [] tiles;
+		int count = 0;
+		while(s.hasNext()){
+			String str = s.next();
+			tiles = str.toCharArray();
+		//	for(int row = 0; row<15; row++){
+				for(int col =0; col<tiles.length; col++){
+					if(tiles[col] == ('W')){
+						itemMap[count][col] =new Wall(new Position(count,col),this);
+					}
+					else if(tiles[col] == ('w')){
+						itemMap[count][col] =new Wall(new Position(count,col),this);
+					}
+					else if(tiles[col] == (' ')){
+						itemMap[count][col] =new Tile(new Position(count,col),this);
+					}
+					else if(tiles[col] == ('q')){
+						itemMap[count][col] = new Pipe(new Position(count,col),this, 'Q');
+					}
+					else if(str.equals(null)){
+						itemMap[count][col] =new Tile(new Position(count,col),this);
+					}
+					
+				}
+				count++;
+			}
+		
+	}
+	
+	@Override
+	public void syncItemMapAndField(ArrayList<Avatar> movableTiles) {
+		super.syncItemMapAndField(movableTiles);
+		// the items should be seen rather than the water
+		Position pos;
+		for (int i = 0; i < movableTiles.size(); i++) {
+			pos = movableTiles.get(i).getPosition();
+			playingField[pos.getRow()][pos.getCol()] = itemMap[pos.getRow()][pos.getCol()];
+		}
+		setChanged();
+		notifyObservers("update");
+	}
+	
+	/**
+	 * Overridden playTurn method for Pipes game. 
+	 * Needed to be overridden completely largely due to the
+	 * multiplying nature of the Water flowing through
+	 * the Pipes.
+	 * @throws GameOver
+	 */
+	@Override
+	public void playTurn(Position position) {
+		numTurns++;
+		turnsUntilWater--;
+		//Hero is always the first element of the ArrayList
+		Plumber plumber = (Plumber) movableTiles.get(0);
+		
+		Position movedPosition = plumber.getNextPosition(position);
+		
+		//check if moved to existing Pipe
+		boolean itemPickedUp = plumber.hasGoodie(movedPosition); 
+		if(!runWater)
+		{
+			if(itemPickedUp){ //player has clicked on a pipe
+				((Item) itemMap[movedPosition.getRow()][movedPosition.getCol()]).pickedUp(plumber);
+				//System.out.println("you rotated the pipe at:" + movedPosition.getRow() + "," + movedPosition.getCol());
+				plumber.setPosition(movedPosition);
+				syncItemMapAndField(movableTiles);
+				if(turnsUntilWater > 0)
+					turnsUntilWater++;
+				return;			
+			}
+			else{ //plumber is placing a pipe
+				plumber.setPosition(movedPosition);
+				plumber.placePipe();
+				plumber.acquireNextPipeType();
+				syncItemMapAndField(movableTiles);
+			}		
+		}
+		
+		if(turnsUntilWater <= 0){
+			Water water;
+			Position[] nextWaterPositions;
+			//need the current number of movable Tiles to loop over
+			int currentMovableTilesSize = movableTiles.size();
+			//now to move the water...
+			for(int i = 1; i < currentMovableTilesSize; i++){
+				water = (Water) movableTiles.get(i); 
+				int currentRow = water.getPosition().getRow();
+				int currentCol = water.getPosition().getCol();
+				//check if the water has hit a pipe
+				if(water.hasGoodie(water.getPosition())){
+					((Item) itemMap[currentRow][currentCol]).pickedUp(water);
+					//pass current position since water movement does not depend on plumber
+					nextWaterPositions = water.getNextPositions(water.getPosition());
+					//water.setPosition(nextWaterPos);
+					//loop over possible next positions for water 
+					for(int j = 0; j < nextWaterPositions.length; j++){
+						if(nextWaterPositions[j] != null){
+							int nextRow = nextWaterPositions[j].getRow();
+							int nextCol = nextWaterPositions[j].getCol();
+							//get the tile at this position
+							Tile tile = itemMap[nextRow][nextCol];
+							//if the tile is a pipe try to fill it
+							if(tile instanceof Pipe){
+								//for each possible opening, also check that the water is
+								//coming from the appropriate place.
+								if((((Pipe) tile).isOpenLeft()) &&
+										(currentRow == nextRow) && (currentCol == nextCol - 1)){
+									movableTiles.add(new Water(new Position(nextRow, nextCol),this));
+								}
+								else if((((Pipe) tile).isOpenBottom()) && 
+										(currentRow == nextRow + 1) && (currentCol == nextCol)){
+									movableTiles.add(new Water(new Position(nextRow, nextCol),this));
+								}
+								else if((((Pipe) tile).isOpenRight()) &&
+										(currentRow == nextRow) && (currentCol == nextCol + 1)){
+									movableTiles.add(new Water(new Position(nextRow, nextCol),this));
+								}
+								else if((((Pipe) tile).isOpenTop()) &&
+										(currentRow == nextRow - 1) && (currentCol == nextCol)){
+									movableTiles.add(new Water(new Position(nextRow, nextCol),this));
+								}
+								//if the pipe cannot be filled (water trying to enter a blocked wall)
+								//it counts as a leak and you lose
+								else{
+									syncItemMapAndField(movableTiles);
+									setChanged();
+									notifyObservers("You have a leak: you lose");
+									runWater = false;
+									return;
+								}
+							}
+							//if the tile is not a pipe the game ends with a win (winningTile) 
+							//or a loss (any other non-pipe tile)
+							else{
+								movableTiles.add(new Water(new Position(nextRow, nextCol),this));
+
+							}
+						}
+					}
+						
+					
+				}
+				else{
+					if (checkWin()) {
+						syncItemMapAndField(movableTiles);
+						if(lastLevel()){
+							setChanged();
+							notifyObservers("Congratulations: You win!");
+						}
+						nextLevel();
+						runWater = false;
+						return;
+					}
+					syncItemMapAndField(movableTiles);
+					setChanged();
+					notifyObservers("You have a leak: you lose");
+					runWater = false;
+					return;
+				}
+			}
+			
+			syncItemMapAndField(movableTiles);
+			if (checkWin()) {
+				syncItemMapAndField(movableTiles);
+				if(lastLevel()){
+					setChanged();
+					notifyObservers("Congratulations: You win!");
+				}
+				nextLevel();
+				runWater = false;
+				return;
+			}
+		}
+	}
+	
+	/**
+	 * Returns the number of turns left until
+	 * water starts to flow.
+	 * @return turnsUntilWater
+	 */
+	public int getTurnsUntilWater(){
+		return turnsUntilWater; 
+	}
+	
+	public void runWaterPressed() {
+		runWater = true;
+		while(runWater){
+			this.playTurn(new Position(1,1));
+		}
+	}
+
+	@Override
+	public void resetPlayingField() {
+	}
+
+	@Override
+	protected boolean checkWin() {
+		Tile winningTile = playingField[winningPosition.getRow()][winningPosition.getCol()];
+		if(winningTile instanceof Pipe) {
+			return ((Pipe) winningTile).isFilled();
+		} return false;
+	}
+	
+	@Override
+	public void restartGame() {
+		numTurns = 0;
+		runWater = false;
+		turnsUntilWater = WATER_START_TURN;
+		this.width = WIDTH;
+		this.height = HEIGHT;
+		playingField = new Tile[height][width];	
+		itemMap = new Tile[height][width];
+		movableTiles = new ArrayList<Avatar>();
+		map=1;
+		populateItemMap();
+		syncItemMapAndField(movableTiles);
+	}
+
+	public void nextLevel() {
+		numTurns = 0;
+		runWater = false;
+		turnsUntilWater = WATER_START_TURN;
+		this.width = WIDTH;
+		this.height = HEIGHT;
+		playingField = new Tile[width][height];	
+		itemMap = new Tile[width][height];
+		movableTiles = new ArrayList<Avatar>();
+		map++;
+		populateItemMap();
+		syncItemMapAndField(movableTiles);
+	}
+	
+	protected boolean lastLevel(){
+		if(map == MAP_ONE){
+			return false;
+		}
+		else if(map == MAP_TWO){
+			return false;
+			}
+		else if(map == MAP_THREE ){
+			return true;
+			}
+		return false;
+		
+	}
+
+}
+
